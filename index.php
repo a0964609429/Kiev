9ceb5f2f64febfa081acd9207e55eebabaefe0bb
a0964609429 <69528311+a0964609429@users.noreply.github.com>
2021-11-30 21:26:11 +0200

    Site Kiev

---
+++ index.php
@@ -0,0 +1,64 @@
+
+
+<?php
+include "f_h/head.html";
+include "places/1Andrew_Descent.php";
+?>
+<a href="detail/Andrew_Descent.php">подробнее</a>
+<br><br><br><br>
+<?php
+include "places/1dom_bylgakava.php";
+?>
+<a href="detail/dom_bylgakava.php">подробнее</a>
+<br><br><br><br>
+<?php
+include "places/1ez_v_tymane.php";
+?>
+<a href="detail/ez_v_tymane.php">подробнее</a>
+<br><br><br><br>
+<?php
+include "places/1Funicular.php";
+include "places/1gidrapark.php";
+include "places/1Golden_Gate.php";
+include "places/1House_with_chimeras.php";
+include "places/1kastel.php";
+include "places/1kievska_krepast.php";
+include "places/1kreshatik.php";
+include "places/1Landscape_alley.php";
+include "places/1myzey_aviazei.php";
+include "places/1myzey_troleibysnava_taloncheka.php";
+include "places/1myzey_ykrain.php";
+include "places/1Park_of_Eternal_Glory.php";
+include "places/1pechersk.php";
+include "places/1pysha.php";
+include "places/1safiiska_ploshad.php";
+include "places/1Shevchenko_park.php";
+include "places/1teatr_shevchenka.php";
+include "places/1Trukhanov_Island.php";
+include "places/1Vladimirskaya_Gorka.php";
+include "places/1xram_na_vade.php";
+include "places/1mixalavski_zolotoverx.php";
+include "places/1rodina_mati.php";
+include "places/1lavra.php";
+include "places/1samson.php";
+ 
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include "f_h/futer.html" ?> 
---
+++ places/1samson.php
@@ -0,0 +1,9 @@
+<h2> Фонтан «Самсон» </h2>
+
+<img src="img/samson.jpg" class="imd">
+
+<p>
+Фонтан «Самсон» — один из старейших фонтанов Киева.
+</p>
+
+<a href="Andrew_Descent.html">подробнее</a>
